63968c1362c98ef0dce6eddef633294c526bcd11
ruaruababa <ruaruababa3333@gmail.com>
2021-09-19 23:05:27 +0700

    upload bai tap thiet ke giao dien web

---
+++ .history/main_20210918162523.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navMeunu = document.querySelector(".nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  nav.classList.remove("active");
+}
